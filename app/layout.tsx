e261e3370e270215bd7955eb3e2d5def693383e6
Paul Aderoju <paul.adeboye@yahoo.com>
2025-10-03 21:57:29 +0100

    fix: some mods to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'DAG Attendance App',
+  description: 'Created by Arthurite Integrated',
+  generator: 'ATR Systems',
 }
 
 export default function RootLayout({
